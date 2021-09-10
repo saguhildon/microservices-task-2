1775c8a7c422c985642195b62d9607409b047f0d
saguhildon <saguhildon.sg@gmail.com>
2021-09-10 16:12:22 +0800

    Add project files.

---
+++ MovieBookingAPI/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieBookingAPI.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookingId { get; set; }
+        public DateTime Date { get; set; }
+        public string Venue { get; set; }
+
+        public string screen { get; set; }
+        public string MovieName { get; set; }
+
+        public int NumberofTickets { get; set; }
+        public int NumberofSeats { get; set; }
+
+    }
+}
---
+++ MovieBookingAPI/Models/BookingContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieBookingAPI.Models;
+
+namespace MovieBookingAPI.Models
+{
+    public class BookingContext : DbContext
+    {
+        public BookingContext(DbContextOptions<BookingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> BookItems { get; set; }
+    }
+}
